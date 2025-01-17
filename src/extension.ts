2ed891628d2c92bb6da9d711903cc59d6dd8867d
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-01-17 01:42:30 +0100

    added analyzers

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,9 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "betterSEO" is now active!');
 	const disposable = vscode.commands.registerCommand('betterSEO.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from me!');
+		vscode.window.showInformationMessage('BetterSEO is Working!');
 	});
 
 	context.subscriptions.push(disposable);
 }
-export function deactivate() {}
+export function deactivate() {};
