883b85d6f3d19ef3a8c607ce65378d4324961bdb
Caleb Kalejaiye <kalejaiyecaleb@gmail.com>
2025-07-29 12:27:43 +0100

    feat: made updates

--- src/utils/geminiUtils.ts
+++ src/utils/geminiUtils.ts
@@ -1,5 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const apiKey = process.env.GEMINI_API_KEY;
 
